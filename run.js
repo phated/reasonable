ccd820113ac320fad047e52323296c72b99aa3d2
Blaine Bublitz <blaine.bublitz@gmail.com>
2018-06-02 19:34:51 -0700

    Regen the compiler with export

--- run.js
+++ run.js
@@ -2,11 +2,14 @@ import { ab2str, str2ab } from 'utils.js';
 import { compiler } from 'compiler.js';
 import { refmt } from 'refmt.js';
 
+var global = {};
+compiler(global);
+
 V8Worker2.recv(function(bytes) {
   var reason = ab2str(bytes);
   // V8Worker2.print(reason);
   var ocaml = refmt.printML(refmt.parseRE(reason));
-  var res = compiler.ocaml.compile_super_errors(ocaml);
+  var res = global.ocaml.compile_super_errors(ocaml);
   if (res.js_code) {
     V8Worker2.send(str2ab(res.js_code));
   } else {
