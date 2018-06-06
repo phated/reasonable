f09cd786b0282790bdcc1d3b436dbcddce769a68
Blaine Bublitz <blaine.bublitz@gmail.com>
2018-06-06 01:35:07 -0700

    Module loading for the bucklescript stdlib

--- example.re
+++ example.re
@@ -1,7 +1,7 @@
 [@bs.scope "V8Worker2"] [@bs.val] external print : string => unit = "print";
 
 /* Based on https://rosettacode.org/wiki/FizzBuzz#OCaml */
-let fizzbuzz = (i) =>
+/* let fizzbuzz = (i) =>
   switch (i mod 3, i mod 5) {
   | (0, 0) => "FizzBuzz"
   | (0, _) => "Fizz"
@@ -11,14 +11,15 @@ let fizzbuzz = (i) =>
 
 for (i in 1 to 100) {
   print(fizzbuzz(i))
-};
+}; */
 
-/*
+
 module Foo = {
   type a = {name: string};
 };
 
-let student = [|{Foo.name: "J"}|];
+let student = [|{Foo.name: "Blaine"}, {Foo.name: "Jake"}, {Foo.name: "Nick"}|];
 
 let a = Belt.Array.map(student, s => s.name);
-*/
+
+Belt.Array.forEach(a, print)
--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/gobuffalo/packr"
 	"github.com/ry/v8worker2"
@@ -51,6 +53,40 @@ func main() {
 			}
 		}
 		return nil
+	})
+
+	moduleNames := make(map[string]string)
+
+	worker.SetModuleResolver(func(moduleName, referrerName string) int {
+		if _, exists := moduleNames[moduleName]; exists {
+			return 0
+		}
+
+		var modulePath string
+		if filepath.IsAbs(moduleName) {
+			modulePath = filepath.Clean(moduleName)
+		} else {
+			referrerDir := filepath.Dir(moduleNames[referrerName])
+			modulePath = filepath.Join(referrerDir, moduleName)
+		}
+		moduleNames[moduleName] = modulePath
+
+		if strings.HasPrefix(modulePath, "stdlib/") == true {
+			code, codeErr := box.MustString(modulePath)
+			if codeErr != nil {
+				return 1
+			}
+
+			if err := worker.LoadModule(moduleName, code); err != nil {
+				return 1
+			}
+		} else {
+			if err := LoadReasonFile(worker, modulePath); err != nil {
+				return 1
+			}
+		}
+
+		return 0
 	})
 
 	if err := LoadModuleByFilename(worker, "compiler.js"); err != nil {
